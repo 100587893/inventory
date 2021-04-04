38b637f75e67c4cb34bee0c3a6948c5ffa59d459
coolguy967 <abdurrahman.ansari@uoit.net>
2021-04-04 12:11:56 -0400

    Update lm_local_inv.php

--- purchasing/lm/lm_local_inv.php
+++ purchasing/lm/lm_local_inv.php
@@ -2,7 +2,7 @@
   session_start();
   if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSSION['role']='local') {
 	include("lm_topbar.php");
-	include("..\..\connect.php");
+	include("../../connect.php");
   } else {
   	header("Location: C:/wamp64/www/index.php");
   	exit();
