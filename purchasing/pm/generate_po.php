2626756cb12144ae7b496dee64b875159f672a23
Mohammed Hameeduddin <45569565+100587893@users.noreply.github.com>
2021-04-05 21:25:43 -0400

    Update generate_po.php

--- purchasing/pm/generate_po.php
+++ purchasing/pm/generate_po.php
@@ -165,7 +165,7 @@
 				<h5 class='total_label' style='float: right;'><b>Order Total:</b> $" .$order_total. "</h5>
 			</div>";
 	
-	$file_location = "PO_$po_id.pdf";
+	$file_location = "../../po/PO_$po_id.pdf";
 
 	$pdf = new \Mpdf\Mpdf();
 
